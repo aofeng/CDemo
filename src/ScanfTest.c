6039b24239a22615a1fc28ea7c98fe9f305e14aa
NieYong <aofengblog@163.com>
2014-08-06 20:12:50 +0800

    scanf方法的使用

---
+++ src/ScanfTest.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/*
+ * 用scanf读取字符。
+ */
+void readChar();
+
+/*
+ * 用scanf读取整数。
+ */
+void readInt();
+
+/*
+ * 用scanf读取字符串。
+ */
+void readString();
+
+int main(int argc, char **argv) {
+//    readChar();
+//    readInt();
+    readString();
+
+    return 0;
+}
+
+void readChar() {
+    char a, b, c;
+    printf("输入三个字符：");
+    scanf("%c%c%c", &a, &b, &c);
+    printf("读取三个字符的结果：a=%c, b=%c, c=%c\n", a, b, c);
+}
+
+void readInt() {
+    int a, b, c;
+    printf("输入三个整数：");
+    scanf("%d%d%d", &a, &b, &c);
+    printf("读取三个整数的结果：a=%d, b=%d, c=%d\n", a, b, c);
+}
+
+void readString() {
+    char a[10], b[5], c[9];
+    printf("输入三个字符串：");
+    scanf("%10s%5s%9s", a, b, c);
+    printf("读取三个字符串的结果：a=%s, b=%s, c=%s\n", a, b, c);
+}
