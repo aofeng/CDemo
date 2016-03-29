dc178350e31eb1cc75222e1eb42b2128b8e604c9
NieYong <aofengblog@163.com>
2016-03-29 17:00:23 +0800

    复制字符串

--- src/std/string/string.c
+++ src/std/string/string.c
@@ -39,7 +39,7 @@ void join() {
     char *character = "abcdefghijklmnopqrstuvwxyz";
 
     size_t size = strlen(digit) + strlen(character);
-    char all[size+1];
+    char all[size + 1];
     memset(all, 0, size);
     strcat(all, digit);
     strcat(all, character);
@@ -47,12 +47,44 @@ void join() {
 }
 
 /**
- * 复制字符串。
+ * 搜索字符串中的字符，返回所在位置编号。
  */
 void search(char *str, char c) {
     char *p = memchr(str, c, strlen(str));
     int index = p - str;
     printf("char '%c' in string \"%s\" index:%d%c", c, str, index, NEW_LINE);
+}
+
+void multiTypeCopy(size_t destSize, char* src) {
+    char dest1[destSize];
+    memset(dest1, 0, destSize);
+    memcpy(dest1, src, destSize);
+    printf("src:%s, use memcpy(dest, src, destSize), dest:%s%c", src, dest1, NEW_LINE);
+
+    char dest2[destSize];
+    memset(dest2, 0, destSize);
+    strncpy(dest2, src, destSize);
+    printf("src:%s, use strncpy(dest, src, destSize), dest:%s%c", src, dest2, NEW_LINE);
+
+    char dest3[destSize];
+    memset(dest3, 0, destSize);
+    strcpy(dest3, src);
+    printf("src:%s, use strcpy(dest, src), dest:%s%c", src, dest3, NEW_LINE);
+}
+
+/**
+ * 不同字符串复制方式比较。
+ */
+void copy() {
+    size_t size = 30;
+
+    char *src1 = "abcdefghijkl0123";
+    printf("字符串不包含\'\\0\'，比较3种不同的复制方式%c", NEW_LINE);
+    multiTypeCopy(size, src1);
+
+    char *src2 = "abcdef\0ghijkl0123";
+    printf("字符串包含\'\\0\'，比较3种不同的复制方式%c", NEW_LINE);
+    multiTypeCopy(size, src2);
 }
 
 int main(int argc, char **argv) {
@@ -61,6 +93,7 @@ int main(int argc, char **argv) {
     search("abcdefghijkl", 'l');
     search("abcdefghijkl", 'a');
     search("1234567890", '4');
+    copy();
 
     return EXIT_SUCCESS;
 }
