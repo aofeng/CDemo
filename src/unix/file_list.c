8d530a655b5b39da806f1911d731c20135416c78
NieYong <aofengblog@163.com>
2016-04-13 17:31:44 +0800

    1、修正不能读取程序执行目录以外的目录的BUG。 2、改用lstat代替stat。

--- src/unix/file_list.c
+++ src/unix/file_list.c
@@ -4,8 +4,11 @@
  *      Author: NieYong<aofengblog@163.com>
  */
 
-#ifndef __USE_BSD
-    #define __USE_BSD
+#ifndef _GNU_SOURCE
+    #define _GNU_SOURCE
+#endif
+#ifndef __USE_XOPEN
+    #define __USE_XOPEN
 #endif
 
 #include <stdlib.h>
@@ -14,6 +17,8 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
 
 /**
  * 将秒数转换成"2016-04-08 18:30:20"格式的字符串。
@@ -25,35 +30,44 @@ void sec2datetimestr(const time_t seconds, char *datetimestr) {
 }
 
 void printDir(char *dir_path) {
-    struct stat fp;
+    struct stat buf;
     char *ftype;
     char ctime[20];
     char mtime[20];
 
+    printf("%-4s %-50s %-22s %-22s\n", "type", "file_name", "ctime", "mtime");
+    printf("----------------------------------------------------------------------------------------------------\n");
+    chdir(dir_path); // 由于opendir传入的参数是目录名，因此必须切换至该目录
     DIR *ds = opendir(dir_path);
     struct dirent *dp = readdir(ds);
-    printf("%-4s %-20s %-22s %-22s\n", "type", "file_name", "ctime", "mtime");
-    printf("----------------------------------------------------------------------\n");
     while (NULL != dp) {
-        stat(dp->d_name, &fp);
-        if (S_ISREG(fp.st_mode)) {
+        if (lstat(dp->d_name, &buf) == -1) { // 定义__USE_XOPEN才能使用lstat，否则只能使用stat
+            fprintf(stderr, "get [%s] inode information fail\n", dp->d_name);
+            strerror(errno);
+            dp = readdir(ds);
+            continue;
+        }
+        if (S_ISREG(buf.st_mode)) {
             ftype = "FILE";
-        } else if (S_ISCHR(fp.st_mode)) {
-            ftype = "CHR";
-        } else if (S_ISFIFO(fp.st_mode)) {
-            ftype = "FIFO";
-        } else if (S_ISLNK(fp.st_mode)) {
-            ftype = "LINK";
-        } else if (S_ISBLK(fp.st_mode)) {
-            ftype = "BLK";
-        } else if (S_ISDIR(fp.st_mode)) {
+        } else if (S_ISDIR(buf.st_mode)) {
             ftype = "DIR";
+        } else if (S_ISCHR(buf.st_mode)) {
+            ftype = "CHR";
+        } else if (S_ISFIFO(buf.st_mode)) {
+            ftype = "FIFO";
+        } else if (S_ISLNK(buf.st_mode)) {
+            ftype = "LINK";
+        } else if (S_ISBLK(buf.st_mode)) {
+            ftype = "BLK";
+        } else if (S_ISSOCK(buf.st_mode)) { // 定义_GNU_SOURCE才能使用S_ISSOCK
+            ftype = "SOCK";
         } else {
             ftype = "UNKO";
         }
-        sec2datetimestr(fp.st_ctime, ctime);
-        sec2datetimestr(fp.st_mtime, mtime);
-        printf("%-4s %-20s %-22s %-22s\n", ftype, dp->d_name, ctime, mtime);
+        sec2datetimestr(buf.st_ctime, ctime);
+        sec2datetimestr(buf.st_mtime, mtime);
+        printf("%-4s %-50s %-22s %-22s\n", ftype, dp->d_name, ctime, mtime);
+
         dp = readdir(ds);
     }
     closedir(ds);
